06276d16130e8565789d59c9dfd4a945cb4dd600
Mohammed-zeeshan <117900168+Mohammed-zeeshan@users.noreply.github.com>
2024-06-26 01:39:48 +0530

    Get the messages from the Database

--- chats.js
+++ chats.js
@@ -4,20 +4,23 @@ const SignUp = require('../models/signup');
 exports.postMessages = async (req, res, next) => {
     const message = req.body.message;
     const signupId = req.user.id;
-    await Chats.create({ message, signupId }).then((message) => {
-        SignUp.findAll({where: {id: req.user.id}}).then((uname) => {
-            return res.status(201).json({message, uname});
+    try {
+        await SignUp.findAll({where: {id: signupId}}).then((uname) => {
+            const userName = uname[0].name;
+            Chats.create({message, userName, signupId}).then((message) => {
+                res.status(201).json(message);
+            }).catch((err) => {
+                console.log(err);
+            })
         })
-    }).catch((err) => {
+    } catch (err) {
         console.log(err);
-    })
+    }
 }
 
 exports.getMessages = async (req, res, next) => {
     await Chats.findAll().then((data) => {
-        return SignUp.findAll({where: {id: data[0].name}}).then((uname) => {
-            res.status(201).json(data);
-        })
+        return res.status(201).json(data);
     }).catch((err) => {
         console.log(err);
     })
